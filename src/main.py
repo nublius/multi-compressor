3614bd33a7a72714edff0de76e43daaecd1b6de0
nublius <184036331+nublius@users.noreply.github.com>
2025-10-31 13:31:58 -0700

    progress bar

--- src/main.py
+++ src/main.py
@@ -1,11 +1,20 @@
 from compress_subfolders import compress_subfolders
+from pathlib import Path
 import click
 
 @click.command()
 @click.argument("path")
 def compressor(path):
     """Compress all subfolders in the given path."""
-    compress_subfolders(path)
+    path = Path(path)
+    subfolders = [f for f in path.iterdir() if f.is_dir()]
+    
+    print(f"Compressing subfolders in {path.name}")
+
+    # Progress bar
+    with click.progressbar(subfolders, label="Compressing...") as bar:
+        for folder in bar:
+            print(f" Compressing {folder.name}...")
 
 if __name__ == "__main__":
     compressor()
